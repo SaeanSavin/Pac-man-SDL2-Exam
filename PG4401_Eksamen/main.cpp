9f790f1efb9840bde33c2d8d49c08dff0e22f6e7
zamFe <zamfel18@student.kristiania.no>
2020-04-04 05:26:16 +0200

    Fixed an error in map selectrion

--- PG4401_Eksamen/main.cpp
+++ PG4401_Eksamen/main.cpp
@@ -7,18 +7,18 @@
 int main(int argc, char *argv[]) {
 
 	std::cout << "Welcome to pac-man\n" << std::endl;
-
 	std::string path = "../maps";
 	std::vector<std::string> mapList{};
 	int index = 1;
 	int mapSelection = 0;
 
-
+	
 	for (const auto &entry : std::filesystem::directory_iterator(path)) {
 		
 		if (entry.path().has_extension()) {
 			std::cout << index << ") " << entry.path().filename() << std::endl;
-			mapList.emplace_back(entry.path());
+			std::cout << "" << std::endl;
+			mapList.emplace_back(entry.path().string());
 			index++;
 		}
 
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 	std::cout << "\n\n\n" << std::endl;
 
 	std::string name = mapList[mapSelection - 1];
-
+	
 	auto gm = std::make_unique<GameManager>();
 
 	gm->play();
