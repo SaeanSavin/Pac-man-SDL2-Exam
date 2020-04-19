ef96c83a25ed29112db61e0a119ec20ba10fa97e
Andreas Sæther <andreassaether94@gmail.com>
2020-04-19 03:01:33 +0200

    Fixed a bug where u cant eat the apple

--- PG4401_Eksamen/GameManager.cpp
+++ PG4401_Eksamen/GameManager.cpp
@@ -144,6 +144,9 @@ void GameManager::play(std::string name) {
 				edible.emplace_back(mapRect);
 				walkable.emplace_back(mapRect);
 				break;
+			case 'A' :
+				edible.emplace_back(mapRect);
+				walkable.emplace_back(mapRect);
 			case 'P':
 				edible.emplace_back(mapRect);
 				walkable.emplace_back(mapRect);
